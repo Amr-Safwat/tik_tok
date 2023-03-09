d6a6c6a6daddbc5f7dbcbd24282650ec5155fcbe
Amr Safwat <amrsafwat02@gmail.com>
2023-03-09 02:12:46 +0200

    Added some Modifiers

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 let videos = document.querySelectorAll('video');
+let icons = document.querySelectorAll('.icon');
 
 for(let video of videos) {
   video.addEventListener('click',()=>{
     if(video.paused) {
       video.play();
+      icons[0].style.display = 'flex';
+      setTimeout(()=> {
+        icons[0].style.display = 'none';
+      },600);
     }else {
       video.pause();
+      icons[1].style.display = 'flex';
+      setTimeout(()=> {
+        icons[1].style.display = 'none';
+      },600);
     }
   })
 }
