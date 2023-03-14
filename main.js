73021c42250b91d251a58be71771c4c88762a62c
Amr Safwat <amrsafwat02@gmail.com>
2023-03-14 14:14:35 +0200

    Added some Modifiers

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 let videos = document.querySelectorAll('video');
 let icon = document.querySelectorAll('.icon');
 let likes = document.getElementsByClassName('like');
-let comments = document.getElementsByClassName('comment');
+let commentsI = document.getElementsByClassName('comment');
 let pAll = document.querySelectorAll('.like + p');
-let commentsList = document.querySelectorAll('.comments');
+let pCom = document.querySelectorAll('.comment + p');
+let comments = document.querySelectorAll('.comments');
+let commentsList = document.querySelectorAll('.comments-list');
+let addCom = document.querySelectorAll('.comments button');
+let input = document.querySelectorAll('.comments input');
+let spans = document.querySelectorAll('.comments > h3 span')
 
 for(let video of videos) {
   video.addEventListener('click',()=>{
@@ -23,7 +28,7 @@ for(let video of videos) {
   })
 }
 
-for(let i = 0;i< comments.length;i++) {
+for(let i = 0;i< commentsI.length;i++) {
   likes[i].onclick = ()=>{
     icon[2].style.display = 'flex';
     setTimeout(()=> {
@@ -32,13 +37,48 @@ for(let i = 0;i< comments.length;i++) {
     pAll[i].innerHTML++;
   }
 }
+let msgTime = new Date();
 
-for(let i = 0;i< comments.length;i++) {
-  comments[i].onclick = ()=>{
-    commentsList[i].style.bottom = '0';
+for(let i = 0;i< commentsI.length;i++) {
+  commentsI[i].onclick = ()=>{
+    comments[i].style.bottom = '0';
   }
   videos[i].onclick = ()=>{
-    commentsList[i].style.bottom = '-60%';
+    comments[i].style.bottom = '-60%';
   }
 }
-// console.log(icon);
+for(let i = 0;i< commentsI.length;i++) {
+  commentsI[i].onclick = ()=>{
+    comments[i].style.bottom = '0';
+  }
+  videos[i].onclick = ()=>{
+    comments[i].style.bottom = '-60%';
+  }
+}
+for(let i = 0;i< addCom.length;i++) {
+  addCom[i].onclick = ()=>{
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    let div = document.createElement('div');
+    let h4 = document.createElement('h4');
+    let p = document.createElement('p');
+    let span = document.createElement('span');
+    let text = document.createTextNode(input[i].value);
+    div.className = 'info';
+    h4.innerText = 'Amr Safwat';
+    img.src = 'icons/Amr.png';
+    span.innerText = `${msgTime.getDate()}/${msgTime.getMonth()}/${msgTime.getFullYear()}`;
+    p.append(text);
+    commentsList[i].appendChild(li);
+    div.appendChild(h4);
+    div.appendChild(p);
+    li.appendChild(img);
+    li.appendChild(div);
+    li.appendChild(span);
+    pCom[i].innerText++;
+    spans[i].innerText++;
+    input[i].value = '';
+  }
+}
+
+// console.log(pCom[3]);
